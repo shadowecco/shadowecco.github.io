ec3433f99879fd370b33aa3a7e47b57a86211d8c
shadowecco <65340161+shadowecco@users.noreply.github.com>
2022-08-12 16:15:23 +0100

    Updating website with new links

---
+++ projects/web/tribute-page-web/js/project.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".project-area").load("./page-elements/project.html");
+  $(".page-contacts").load(
+    "../../../assets/page-elements/main.html #page-contacts"
+  );
+});

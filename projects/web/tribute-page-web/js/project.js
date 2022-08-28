c6104dc55fe3ef45089460bf52c64bd117edaa95
shadowecco <yathelen@hotmail.com>
2022-08-28 22:44:37 +0100

    Removing all old files
    Removing all old files

--- projects/web/tribute-page-web/js/project.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".project-area").load("./page-elements/project.html");
-  $(".page-contacts").load(
-    "../../../assets/page-elements/main.html #page-contacts"
-  );
-});
